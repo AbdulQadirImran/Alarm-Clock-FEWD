839b9ba86cb0957f43fb57f185c51f5fa9171822
AbdulQadirImran <160231143+AbdulQadirImran@users.noreply.github.com>
2024-03-03 18:38:54 +0500

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const currentTime = document.querySelector("h1"),
 content = document.querySelector(".content"),
 selectMenu = document.querySelectorAll("select"),
 setAlarmBtn = document.querySelector("button");
-
 let alarmTime, isAlarmSet,
 ringtone = new Audio("./files/ringtone.mp3");
 
